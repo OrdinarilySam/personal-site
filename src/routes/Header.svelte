aae44fb649e17741ffed8655f8040d81ceee5f2d
OrdinarilySam <56322367+OrdinarilySam@users.noreply.github.com>
2023-06-04 00:51:36 -0500

    started with home page layout and header

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -2,11 +2,30 @@
 </script>
 
 <header>
+  <nav>
+    <ul>
+      <li>Home</li>
+      <li>Projects</li>
+      <li>Photography</li>
+      <li>About</li>
+    </ul>
+  </nav>
 </header>
 
 <style>
 	header {
+    width: 100%;
 		display: flex;
 		justify-content: space-between;
 	}
+  nav {
+    width: 100%;
+  }
+  ul {
+    display: flex;
+    justify-content: space-between;
+  }
+  li {
+    list-style-type: none;
+  }
 </style>
