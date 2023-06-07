32988902c253ff08e8eb872c7a062e94061a16ac
OrdinarilySam <56322367+OrdinarilySam@users.noreply.github.com>
2023-06-07 11:06:40 -0500

    added secret to about page

---
+++ src/lib/components/UnderConstruction.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  export let pageName = "This page"
+</script>
+
+<h1></h1>
+
+<style lang="scss">
+
+</style>
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -9,30 +9,50 @@
 
 <div class="container">
   <div class="main">
-    <h1>Sam Martin</h1>
-    <p>More coming soon...</p>
-  </div>  
+    <p>Hi, I'm <span>Sam Martin</span>.</p>
+    
+      </div>  
   <div class="side">
     <img src={MeImage} alt="headshot of sam"/>
+    <p aria-hidden="true">Hey there</p>
   </div>
 </div>
 
 <style lang="scss">
   .container {
+    flex-grow: 1;
     display: flex;
+    margin: 1rem;
     flex-direction: row;
-    div {
-      width: 100%;
-    }
+    gap: 1rem;
     .main {
+      flex: 2;
       text-align: center;
-      h1 {
+      p {
         font-size: 3rem;
+        span {
+          font-weight: 600;
+        }
       }
+    }
+    .side {
+      flex: 1;
+      p {
+        position: absolute;
+        transform: rotate(45deg) translate(-3em, -4em);
+        z-index: -1;
+        font-weight: 600;
+              }
     }
 
   }
   img {
-    width: 100%; 
+    max-width: 100%;
+    max-height: 100%;
+    border: 4px solid black;
+    transition: transform 0.5s ease-in-out;
+    &:hover {
+      transform: rotate(45deg);
+    }
   }
 </style>
