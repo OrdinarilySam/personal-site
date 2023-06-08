4571c5784a152cb2b28e345bb033ca79c7f0f0a3
OrdinarilySam <56322367+OrdinarilySam@users.noreply.github.com>
2023-06-08 16:11:47 -0500

    started working on card hover effect

--- src/routes/photography/+page.svelte
+++ src/routes/photography/+page.svelte
@@ -2,3 +2,22 @@
 	<title>Photography</title>
 	<meta name="photography" content="ordinarilysam photography portfolio" />
 </svelte:head>
+
+<script lang="ts">
+  import Card from "./Card.svelte"
+
+</script>
+<div>
+  <Card />
+
+</div>
+<style lang="scss">
+  div {
+    flex-grow: 1;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  
+
+</style>
---
+++ src/routes/photography/Card.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+  let shadow: HTMLDivElement;
+  let card: HTMLDivElement;
+  function handleMove(e: MouseEvent) {
+    // let x: number = 3;
+    // let y: number = 3;
+    //
+    // if ((e.offsetX < 200 && e.offsetY < 200) || (e.offsetX > 200 && e.offsetY > 200)) {
+    //   x = -3;
+    //   y = -3;
+    // }
+    // card.style.transform = `skew(${x}deg, ${y}deg)`
+    
+
+
+    // card.style.boxShadow = `${e.offsetX}px 0 8px -24px inset`
+    // card.style.transform = `rotate(${Math.atan2(e.offsetX, e.offsetY) * 180 / Math.PI}deg)`
+
+    // console.log("degrees:", Math.atan(e.offsetY / e.offsetX) * 180 / Math.PI)
+    // console.log("(", e.offsetX, ", ", e.offsetY, ")")
+
+
+
+  }
+
+</script>
+<div class="content" bind:this={card}>
+  <div class="shadow" bind:this={shadow}>
+
+  </div>
+  <div class="mouse-container" on:mousemove={handleMove} on:mouseleave={() => {card.style.transform = "none"}}></div>
+</div>
+<style lang="scss">
+  .content {
+    height: 400px;
+    width: 400px;
+    position: relative;
+    overflow: hidden;
+    border: 2px solid black;
+    background: lightblue;
+    transition: transform 0.2s;
+    div {
+      position: absolute;
+      width: 100%;
+      height: 100%;
+    }
+  }
+
+</style>
