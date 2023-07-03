e27235158ab90d7596e854596e1fae2888c9975c
OrdinarilySam <56322367+OrdinarilySam@users.noreply.github.com>
2023-07-03 17:24:51 -0500

    removed unnecessary styling and html

--- src/routes/photography/Card.svelte
+++ src/routes/photography/Card.svelte
@@ -4,8 +4,6 @@
 
 <div class="content">
 	<img src={data.url} alt={data.alt} />
-	<div class="reflection" />
-	<div class="shadow" />
 </div>
 
 <style lang="scss">
@@ -23,22 +21,6 @@
 		}
 		&:hover {
 			scale: 1.05;
-		}
-		.reflection {
-			position: absolute;
-			top: 0;
-			left: 0;
-			width: 100%;
-			height: 100%;
-			z-index: 9;
-			transition: all 250ms ease-out;
-		}
-		.shadow {
-			position: absolute;
-			inset: -5px;
-			transform: translate(10px, 8px);
-			z-index: -1;
-			filter: blur(10px);
 		}
 	}
 </style>
