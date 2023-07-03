80d9f88e8f1d93368cc85df5ae7e66dfd411de7a
OrdinarilySam <56322367+OrdinarilySam@users.noreply.github.com>
2023-07-03 17:20:54 -0500

    duplicated photo to work on gallery styling

--- src/lib/data/photos.ts
+++ src/lib/data/photos.ts
@@ -2,5 +2,29 @@ export const photos = [
 	{
 		url: 'https://i.imgur.com/O8O0TH9.jpg',
 		alt: 'northern lights'
+	},
+	{
+		url: 'https://i.imgur.com/O8O0TH9.jpg',
+		alt: 'northern lights'
+	},
+	{
+		url: 'https://i.imgur.com/O8O0TH9.jpg',
+		alt: 'northern lights'
+	},
+	{
+		url: 'https://i.imgur.com/O8O0TH9.jpg',
+		alt: 'northern lights'
+	},
+	{
+		url: 'https://i.imgur.com/O8O0TH9.jpg',
+		alt: 'northern lights'
+	},
+	{
+		url: 'https://i.imgur.com/O8O0TH9.jpg',
+		alt: 'northern lights'
+	},
+	{
+		url: 'https://i.imgur.com/O8O0TH9.jpg',
+		alt: 'northern lights'
 	}
 ];
