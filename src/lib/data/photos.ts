3e95525e920093320a7de6cff4a5f9a209f716b0
OrdinarilySam <56322367+OrdinarilySam@users.noreply.github.com>
2023-07-04 14:13:28 -0500

    updated to have more photos of different sizes

--- src/lib/data/photos.ts
+++ src/lib/data/photos.ts
@@ -1,42 +1,80 @@
 export const photos = [
 	{
 		imageUrl: 'https://i.imgur.com/O8O0TH9.jpg',
-		description: 'Northern Lights',
-		title: 'Northern Lights'
+		title: 'Northern Lights',
+		location: 'Minnesota',
+		date: '2023-04-23'
 	},
 	{
-		imageUrl: 'https://i.imgur.com/O8O0TH9.jpg',
-		description: 'Northern Lights',
-		title: 'Northern Lights'
+		imageUrl: 'https://i.imgur.com/1VsBuqY.jpg',
+		title: 'Deer',
+		location: 'Apple Valley, MN',
+		date: '2023-05-03'
 	},
 	{
-		imageUrl: 'https://i.imgur.com/O8O0TH9.jpg',
-		description: 'Northern Lights',
-		title: 'Northern Lights'
+		imageUrl: 'https://i.imgur.com/d5Oc8a5.jpg',
+		title: '',
+		location: '',
+		date: ''
 	},
 	{
-		imageUrl: 'https://i.imgur.com/O8O0TH9.jpg',
-		description: 'Northern Lights',
-		title: 'Northern Lights'
+		imageUrl: 'https://i.imgur.com/XwyAQr7.jpg',
+		title: '',
+		location: '',
+		date: ''
 	},
 	{
-		imageUrl: 'https://i.imgur.com/O8O0TH9.jpg',
-		description: 'Northern Lights',
-		title: 'Northern Lights'
+		imageUrl: 'https://i.imgur.com/EQyHLeP.jpg',
+		title: '',
+		location: '',
+		date: ''
 	},
 	{
-		imageUrl: 'https://i.imgur.com/O8O0TH9.jpg',
-		description: 'Northern Lights',
-		title: 'Northern Lights'
+		imageUrl: 'https://i.imgur.com/8fbkI04.jpg',
+		title: '',
+		location: '',
+		date: ''
 	},
 	{
-		imageUrl: 'https://i.imgur.com/O8O0TH9.jpg',
-		description: 'Northern Lights',
-		title: 'Northern Lights'
+		imageUrl: 'https://i.imgur.com/PT8T8G1.jpg',
+		title: '',
+		location: '',
+		date: ''
 	},
 	{
-		imageUrl: 'https://i.imgur.com/O8O0TH9.jpg',
-		description: 'Northern Lights',
-		title: 'Northern Lights'
+		imageUrl: 'https://i.imgur.com/ZsSVr6v.jpg',
+		title: '',
+		location: '',
+		date: ''
+	},
+	{
+		imageUrl: 'https://i.imgur.com/6eMdiGJ.jpg',
+		title: '',
+		location: '',
+		date: ''
+	},
+	{
+		imageUrl: 'https://i.imgur.com/fUSzY98.jpg',
+		title: '',
+		location: '',
+		date: ''
+	},
+	{
+		imageUrl: 'https://i.imgur.com/1YMaZaS.jpg',
+		title: '',
+		location: '',
+		date: ''
 	}
+	// {
+	// 	imageUrl: '',
+	// 	title: '',
+	// 	location: '',
+	// 	date: ''
+	// },
+	// {
+	// 	imageUrl: '',
+	// 	title: '',
+	// 	location: '',
+	// 	date: ''
+	// }
 ];
