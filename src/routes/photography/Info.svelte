6624cdab9d6ce23a665e78f247ec58d960349c2c
OrdinarilySam <56322367+OrdinarilySam@users.noreply.github.com>
2023-07-04 13:58:00 -0500

    Info contains relevant data

---
+++ src/routes/photography/Info.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import type { Photo } from '$lib/types';
+	export let data: Photo;
+</script>
+
+<div class="info">
+	<h2>{data.title}</h2>
+	<p>{data.location}</p>
+	<p>{data.date}</p>
+	<a href={data.imageUrl} target="_blank">View Full Size</a>
+</div>
