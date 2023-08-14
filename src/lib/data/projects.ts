46fcff9dab360db0e8e76d05d68aaf6f3700710c
OrdinarilySam <56322367+OrdinarilySam@users.noreply.github.com>
2023-08-14 17:04:02 -0400

    updated template data

--- src/lib/data/projects.ts
+++ src/lib/data/projects.ts
@@ -1,14 +1,24 @@
 export const projects = [
 	{
-		slug: 'template1',
-		title: 'Template 1',
-		description: 'this is the first template',
-		content: '<h1>Template 1</h1><p>Some content</p>'
+		title: 'Project 1',
+		slug: 'project-1',
+		description: 'This is a project',
+		summary: 'This is a project about x, y, and z. It is a project that does a, b, and c.',
+		tech: ['React', 'TypeScript', 'GraphQL', 'Node.js', 'PostgreSQL'],
+		conclusion: 'This project was a success because of x, y, and z.',
+		duration: '1 month',
+		challenges: 'This project was challenging because of x, y, and z.'
 	},
 	{
-		slug: 'template2',
-		title: 'Template 2',
-		description: 'this is the second template',
-		content: '<h1>Template 2</h1><p>Some content</p>'
+		title: 'Project 2',
+		slug: 'project-2',
+		description: 'This is another project',
+		summary: 'This is a very cool project that helped millions and was really cool to make.',
+		tech: ['Svelte', 'TypeScript', 'GraphQL', 'Node.js', 'PostgreSQL'],
+		conclusion:
+			'This project helped me learn about x, y, and z and was able to help 900 monthly users.',
+		duration: '6 months',
+		challenges:
+			'It took me a while to get the x just right, but it was worth it in the end because it really helped the y.'
 	}
 ];
